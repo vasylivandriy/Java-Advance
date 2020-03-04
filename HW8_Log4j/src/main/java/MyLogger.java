787bde99ff5359e39da3dd76903e978963aad205
vasylivandriy <vasylivandriy@gmail.com>
2020-03-04 02:49:42 +0200

    add pom,  Log4j, gitignore, MyLogger.java, SalaryManager.java  files

---
+++ HW8_Log4j/src/main/java/MyLogger.java
@@ -0,0 +1,13 @@
+
+public class MyLogger {
+
+
+    public static void main(String[] args) {
+
+
+        SalaryManager salaryManager = new SalaryManager();
+
+        salaryManager.salary(20, -80, "Anjei");
+    }
+
+}
