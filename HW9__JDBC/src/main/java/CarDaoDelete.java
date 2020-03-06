1dd817c9bf460a27be104dfbf713438151ce6fa5
vasylivandriy <vasylivandriy@gmail.com>
2020-03-06 16:07:44 +0200

    Change CarDaoDelete.java (delete by id) and MAIN.java

--- HW9__JDBC/src/main/java/CarDaoDelete.java
+++ HW9__JDBC/src/main/java/CarDaoDelete.java
@@ -1,2 +1,32 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarDaoDelete {
+
+    private Connection connection;
+    private String DELETE_FROM_TABLE_ID = "DELETE FROM car_table where id = ?";
+    private String DELETE_FROM_TABLE_COLOR = "DELETE FROM car_table where id<>0 and color = ?";
+    private String DELETE_FROM_TABLE_Model = "DELETE FROM car_table where id<>0 and model = ?";
+    private static String select_ID1_from_car_table = "select * from car.car_table";
+
+
+    public CarDaoDelete() throws SQLException {
+        this.connection = ConnectionManager.getConnection();
+    }
+
+
+    public void deleteCond(int id) throws SQLException {
+
+        PreparedStatement preparedStatementID = connection.prepareStatement(DELETE_FROM_TABLE_ID);
+        PreparedStatement preparedStatementColor = connection.prepareStatement(DELETE_FROM_TABLE_COLOR);
+        PreparedStatement preparedStatementModel = connection.prepareStatement(DELETE_FROM_TABLE_Model);
+
+        preparedStatementID.setInt(1, id);
+        preparedStatementID.executeUpdate();
+
+
+    }
+
+
 }
--- HW9__JDBC/src/main/java/MAIN.java
+++ HW9__JDBC/src/main/java/MAIN.java
@@ -5,14 +5,20 @@ public class MAIN {
 
     public static void main(String[] args) throws SQLException {
 
+
+
+        CarDaoInsert carDaoInsert = new CarDaoInsert();
+        carDaoInsert.insertValue("Jaguar", "Red");
+
+
+        CarDaoDelete carDaoDelete = new CarDaoDelete();
+        carDaoDelete.deleteCond(1);
+
         CarDaoRead carDaoRead = new CarDaoRead();
         List<Car> carsRead = carDaoRead.getALL();
 
         System.out.println("Cars read");
         System.out.println(carsRead);
-
-        CarDaoInsert carDaoInsert = new CarDaoInsert();
-        carDaoInsert.insertValue("Jaguar", "Red");
 
 
     }
